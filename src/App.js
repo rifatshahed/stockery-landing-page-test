83979698f9a981a30c0570ff124747ef55ff7b22
rifatshahed <mohammadrifatahmedshahed@gmail.com>
2023-01-17 19:43:54 +0600

    navbar updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Routes/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
